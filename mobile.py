5a6649e1bc0f1aafbcc04ab4be810ccb3c22de1a
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-17 16:20:42 +0500

    cart

--- mobile.py
+++ mobile.py
@@ -18,9 +18,51 @@ class Mobile:
     def total(self) -> float:
         return self.price * self.quantity
 
+    def delete(self, count: int) -> float:
+        self.quantity -= count
+        return self.quantity
 
-m1 = Mobile('Samsung s21', 'black', 700.00, 6, 34)
-m2 = Mobile('Ihone 12 pro', 'gray', 600.00, 4, 21)
 
-print(Mobile.total_price)
-print(Mobile.instances)
+class CartItem:
+    def __init__(self, mobile: Mobile, quanitity: int):
+        self.mobile = mobile
+        self.quanitity = quanitity
+
+    def total(self):
+        return self.mobile.price * self.quanitity
+
+class Cart:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item: CartItem):
+        item.mobile.delete(item.quanitity)
+        self.items.append(item)
+
+    def check(self):
+        data = "Your Cart\n\n"
+        total = 0
+        number = 1
+        for item in self.items:
+            total += item.total()
+            data += f"{number}. {item.mobile.name} X {item.quanitity} = {item.total()}\n"
+            number += 1
+
+        data += f"\nTotal: {total}"
+
+        return data
+
+
+
+m1 = Mobile(name='Samsung S21', color='rad', price=350, ram=4, quantity=34)
+m2 = Mobile(name='Iphone 12 pro', color='black', price=740, ram=6, quantity=25)
+m3 = Mobile(name='Redme 7', color='black', price=250, ram=4, quantity=53)
+
+item1 = CartItem(m1, 3)
+item2 = CartItem(m3, 2)
+
+cart = Cart()
+cart.add_item(item1)
+cart.add_item(item2)
+
+print(cart.check())
